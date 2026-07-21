f314b11122178248fddc3f45b06e20026f3ef3f7
Hudson191-max <djdries122@gmail.com>
2026-07-21 14:04:55 +0200

    fix: rename prisma export to db to match all imports

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  db: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const db = globalForPrisma.db ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.db = db
